5c76c783616aa5836c964785771a4dbc6581a976
YujinLee <yujin092444@gmail.com>
2021-01-01 18:08:29 +0900

    Poster 컴포넌트 Home,TV,Search에 Rendering

--- yuflix/src/Routes/Home/HomePresenter.js
+++ yuflix/src/Routes/Home/HomePresenter.js
@@ -14,16 +14,46 @@ const HomePresenter = ({ nowPlaying, upComing, popular, error, loading }) => loa
 <Container>
     {nowPlaying && nowPlaying.length > 0 && 
     <Section title="Now Playing">
-            {nowPlaying.map(movie => <Poster />)}
+            {nowPlaying.map(movie => (
+                <Poster 
+                    key={movie.id} 
+                    id={movie.id}
+                    title={movie.original_title} 
+                    imgUrl={movie.poster_path}
+                    rating={movie.vote_average}
+                    isMovie={true}
+                    year={movie.release_date && movie.release_date.substring(0,4)}
+                />
+            ))}
     </Section>}
     {upComing && upComing.length > 0 && (
         <Section title="UpComing Movies">
-            {upComing.map(movie => <span key={movie.id}>{movie.title}</span>)}
+            {upComing.map(movie => (
+                <Poster 
+                    key={movie.id} 
+                    id={movie.id}
+                    title={movie.original_title} 
+                    imgUrl={movie.poster_path}
+                    rating={movie.vote_average}
+                    isMovie={true}
+                    year={movie.release_date && movie.release_date.substring(0,4)}
+                />
+            ))}
         </Section>
     )}
     {popular && popular.length > 0 && (
         <Section title="Popular Movies">
-            {popular.map(movie => <span key={movie.id}>{movie.title}</span>)}
+            {popular.map(movie => (
+                <Poster 
+                    key={movie.id} 
+                    id={movie.id}
+                    title={movie.original_title} 
+                    imgUrl={movie.poster_path}
+                    rating={movie.vote_average}
+                    isMovie={true}
+                    year={movie.release_date && movie.release_date.substring(0,4)}
+                />
+            ))}
         </Section>
     )}
     {error && <Message color ="#e74c3c" text={error} />}
