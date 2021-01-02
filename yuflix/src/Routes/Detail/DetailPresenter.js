313102c405aa537797422995ab5fccae6fa099ca
YujinLee <yujin092444@gmail.com>
2021-01-02 23:03:11 +0900

    Detail 컴포넌트 구현 - data 넣기

--- yuflix/src/Routes/Detail/DetailPresenter.js
+++ yuflix/src/Routes/Detail/DetailPresenter.js
@@ -37,8 +37,37 @@ const Backdrop = styled.div`
     background-image: url(${props => props.bgImage});
     background-size: cover;
     background-position: center center;
-    filter: blur(2px);
-    opacity: 0.5;
+    filter: blur(3px);
+    opacity: 0.3;
+`;
+
+const Data = styled.div`
+    width: 70%;
+    margin-left: 40px;
+`;
+
+const Title = styled.h3`
+    font-size: 32px;
+    margin-bottom: 10px;
+`;
+
+const ItemContainer = styled.div`
+    margin: 20px 0;
+`;
+
+const Item = styled.span`
+    font-size: 18px;
+`;
+
+const Divider = styled.span`
+    margin: 0 10px;
+`;
+
+const Overview = styled.p`
+    font-size: 24px;
+    opacity: 0.3;
+    line-height: 1.5;
+    width: 50%;
 `;
 
 const DetailPresenter = ({ result, error, loading }) => (
@@ -54,6 +83,31 @@ const DetailPresenter = ({ result, error, loading }) => (
                         result.poster_path ? `https://image.tmdb.org/t/p/original/${result.poster_path}`
                         : "/noPosterSmall.PNG"
                     } />
+                <Data>
+                    <Title>{result.original_title ? 
+                        result.original_title : 
+                        result.original_name}</Title>    
+                    <ItemContainer>
+                        <Item>{result.release_date ? 
+                            result.release_date.substring(0,4) : 
+                            result.first_air_date.substring(0,4)}
+                        </Item>
+                        <Divider>ㆍ</Divider>
+                        <Item>{result.runtime ? 
+                            result.runtime : 
+                            result.episode_run_time[0]}분
+                        </Item>
+                        <Divider>ㆍ</Divider>
+                        <Item>
+                            {result.genres &&
+                            result.genres.map((genres, index) => 
+                                index === result.genres.length -1 ?
+                                ` ${genres.name}` :
+                                `${genres.name} /`)}
+                        </Item>
+                    </ItemContainer>
+                    <Overview>{result.overview}</Overview>
+                </Data>    
             </Content>
         </Container>
     )
